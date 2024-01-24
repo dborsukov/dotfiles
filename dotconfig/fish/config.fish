58eda79a3f4d33a9f85d7f1f28571bb00a68301d
Denis Borsukov <borsukov@proton.me>
2024-01-24 11:46:57 +0200

    cleaned up

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -1,53 +1,7 @@
 fish_vi_key_bindings
 
-abbr -a lf '~/.config/lf/lf-img'
+function fish_greeting;end
 
-if command -v lazygit > /dev/null
-    abbr -a lg 'lazygit'
-end
-
-if command -v nsxiv > /dev/null
-    abbr -a iv 'nsxiv'
-end
-
-if command -v trash > /dev/null
-    abbr -a rm 'trash'
-end
-
-if command -v exa > /dev/null
-    abbr -a ls 'exa --group-directories-first'
-    abbr -a ll 'exa -l --group-directories-first'
-    abbr -a lla 'exa -la --group-directories-first'
-else
-    abbr -a ls 'ls'
-    abbr -a ll 'ls -l'
-    abbr -a lla 'ls -la'
-end
-
-if test -f /usr/share/autojump/autojump.fish;
-    source /usr/share/autojump/autojump.fish;
-end
-
-function bintopath
-    if test (count $argv) -gt 0;
-        if ! test -f (pwd)/$argv[1];
-            echo "No such file."
-            return
-        end
-        if test (count $argv) -eq 1;
-            ln -sf "$(pwd)/$argv[1]" "$HOME/.bin/"
-            echo "Added."
-            exa -l $HOME/.bin | grep -E "|$argv[1]" --color='always'
-        end
-        if test (count $argv) -eq 2;
-            ln -sf "$(pwd)/$argv[1]" "$HOME/.bin/$argv[2]"
-            echo "Added as '$argv[2]'."
-            exa -l $HOME/.bin | grep -E "|$argv[2]" --color='always'
-        end
-    end
-end
-
-# Fish prompt
 function fish_prompt
     set_color grey
     echo -n "["(date "+%H:%M")"] "
@@ -66,15 +20,40 @@ function fish_prompt
     set_color normal
 end
 
-function fish_greeting 
+# path
+fish_add_path  $HOME/.bin
+fish_add_path  $HOME/.cargo/bin
+fish_add_path  $HOME/.go/bin
+fish_add_path  $HOME/.local/bin
+fish_add_path  $HOME/.pub-cache/bin
+fish_add_path  $HOME/.config/rofi/scripts
+
+# aliases & abbreviations
+abbr -a lf '~/.config/lf/lf-img'
+if command -v nsxiv > /dev/null; abbr -a iv 'nsxiv'; end
+if command -v trash > /dev/null; abbr -a rm 'trash'; end
+if command -v lazygit > /dev/null; abbr -a lg 'lazygit'; end
+if command -v exa > /dev/null
+    abbr -a ls 'exa --group-directories-first'
+    abbr -a ll 'exa -l --group-directories-first'
+    abbr -a lla 'exa -la --group-directories-first'
+else
+    abbr -a ls 'ls'
+    abbr -a ll 'ls -l'
+    abbr -a lla 'ls -la'
+end
+if test -f /usr/share/autojump/autojump.fish;
+    source /usr/share/autojump/autojump.fish;
 end
 
-# XDG
-export XDG_CACHE_HOME=$HOME/.cache
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_DATA_HOME=$HOME/.local/share
-export XDG_STATE_HOME=$HOME/.local/state
-
+# environment
+set -x EDITOR 'nvim'
+set -x VISUAL 'nvim'
+# xdg
+set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_STATE_HOME $HOME/.local/state
 # xdg-ninja
 set -x ANDROID_HOME $XDG_DATA_HOME/android
 set -x GNUPGHOME $XDG_DATA_HOME/gnupg
@@ -87,48 +66,25 @@ set -x PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
 set -x WINEPREFIX $XDG_DATA_HOME/wine
 set -x XAUTHORITY $XDG_RUNTIME_DIR/Xauthority
 set -x _JAVA_OPTIONS -Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java
-
-# FZF
-set -x FZF_DEFAULT_COMMAND 'fd --type file --follow'
-set -x FZF_CTRL_T_COMMAND 'fd --type file --follow'
-set -x FZF_DEFAULT_OPTS '--height 20%'
-
-# Common env
-set -x EDITOR 'nvim'
-set -x VISUAL 'nvim'
-
-# Rust env
+# rust
 set -x RUST_BACKTRACE 1
 set -x CARGO_TARGET_DIR $HOME/.cargo_target_dir
-
-# Go env
+# go
 set -x GOPATH $HOME/.go
-
-# Nvm
+# nvm
 set -x nvm_default_version latest
-
-# Bun
+# bun
 set -x BUN_INSTALL "$HOME/.bun"
 set -x PATH $BUN_INSTALL/bin $PATH
-
 # pnpm
 set -gx PNPM_HOME "/home/db/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
-
 # sxhkd
 set -x SXHKD_SHELL /usr/bin/bash
 
-# Path
-fish_add_path  $HOME/.bin
-fish_add_path  $HOME/.cargo/bin
-fish_add_path  $HOME/.go/bin
-fish_add_path  $HOME/.local/bin
-fish_add_path  $HOME/.pub-cache/bin
-fish_add_path  $HOME/.config/rofi/scripts
-
-# Autostart X
+# autostart x
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
         exec startx -- -keeptty
