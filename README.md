3942c83e1275ef0772fe3f8222d11099bfde3719
Denis Borsukov <borsukov@proton.me>
2024-05-02 16:25:01 +0300

    ditched flavours

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-This repo uses [stow][] to manage dotfiles.  
-Some files are generated dynamically by [flavours][].  
-All important commands can be found in the `Justfile`.
+GNU [stow][] is used to manage dotfiles.
+See `Justfile` for relevant commands.
 
 [stow]: https://www.gnu.org/software/stow/
-[flavours]: https://github.com/Misterio77/flavours
--- xdg_config/nvim/lua/plugins/heirline.lua
+++ xdg_config/nvim/lua/plugins/heirline.lua
@@ -6,49 +6,18 @@ return {
   config = function()
     vim.o.showmode = false
 
-    local utils = require('heirline.utils')
     local devicons = require('nvim-web-devicons')
     local conditions = require('heirline.conditions')
 
-    local colors = {
-      fg = utils.get_highlight('NormalFloat').fg,
-      bg = utils.get_highlight('NormalFloat').bg,
-    }
+    local config = vim.fn['gruvbox_material#get_configuration']()
+    local gruvbox_material_palette = vim.fn['gruvbox_material#get_palette'](config.background, config.foreground, config.colors_override)
+    local palette = function(name)
+      return gruvbox_material_palette[name][1]
+    end
 
-    local SPACE = { provider = ' ' }
-    local SPRING = { provider = '%=' }
-
-    local ViMode = {
+    local Mode = {
       init = function(self)
         self.mode = vim.fn.mode(1):sub(1, 1):lower()
-      end,
-
-      static = {
-        modes = {
-          n = { name = 'N', color = utils.get_highlight('Character').fg },
-          i = { name = 'I', color = utils.get_highlight('Special').fg },
-          v = { name = 'V', color = utils.get_highlight('Repeat').fg },
-          ['\22'] = { name = 'V', utils.get_highlight('Repeat').fg },
-          c = { name = 'C', color = utils.get_highlight('Keyword').fg },
-        },
-      },
-
-      provider = function(self)
-        local mode = self.modes[self.mode]
-        if mode then
-          return ' ' .. mode.name .. ' '
-        else
-          return ' ?' .. self.mode .. '? '
-        end
-      end,
-
-      hl = function(self)
-        local mode = self.modes[self.mode]
-        if mode then
-          return { fg = 'bg', bg = mode.color, bold = true }
-        else
-          return { fg = 'white', bg = 'black', bold = true }
-        end
       end,
 
       update = {
@@ -58,60 +27,79 @@ return {
           vim.cmd('redrawstatus')
         end),
       },
+
+      static = {
+        modes = {
+          n = { name = 'N', color = palette('green') },
+          i = { name = 'I', color = palette('yellow') },
+          v = { name = 'V', color = palette('blue') },
+          ['\22'] = { name = 'V', color = palette('blue') },
+          c = { name = 'C', color = palette('purple') },
+        },
+      },
+
+      hl = function(self)
+        local mode = self.modes[self.mode]
+        if mode then
+          return { fg = palette('bg1'), bg = mode.color, bold = true }
+        else
+          return { fg = palette('fg0'), bg = palette('bg_dim'), bold = true }
+        end
+      end,
+
+      provider = function(self)
+        local mode = self.modes[self.mode]
+        if mode then
+          return ' ' .. mode.name .. ' '
+        else
+          return ' ?' .. self.mode .. '? '
+        end
+      end,
     }
 
-    local FileNameBlock = {
+    local FileBlock = {
       init = function(self)
         self.filename = vim.api.nvim_buf_get_name(0)
+        self.extension = vim.fn.fnamemodify(self.filename, ':e')
+        self.icon, self.icon_color = devicons.get_icon_color(self.filename, self.extension, { default = true })
       end,
-    }
 
-    local FileIcon = {
-      init = function(self)
-        local filename = self.filename
-        local extension = vim.fn.fnamemodify(filename, ':e')
-        self.icon, self.icon_color = devicons.get_icon_color(filename, extension, { default = true })
-      end,
-      provider = function(self)
-        return self.icon and (self.icon .. ' ')
-      end,
-      hl = function(self)
-        return { fg = self.icon_color }
-      end,
-    }
-
-    local FileName = {
-      provider = function(self)
-        local filename = vim.fn.fnamemodify(self.filename, ':.')
-        if filename == '' then
-          return '[No Name]'
-        end
-        if not conditions.width_percent_below(#filename, 0.4) then
-          filename = vim.fn.pathshorten(filename)
-        end
-        return filename
-      end,
-      hl = { fg = utils.get_highlight('NormalFloat').fg },
-    }
-
-    local FileFlags = {
       {
-        condition = function()
-          return vim.bo.modified
+        hl = function(self)
+          return { fg = self.icon_color }
         end,
-        provider = ' [+]',
-        hl = { fg = utils.get_highlight('DiffChange').fg },
+        provider = function(self)
+          return self.icon and (self.icon .. ' ')
+        end,
       },
       {
-        condition = function()
-          return not vim.bo.modifiable or vim.bo.readonly
+        provider = function(self)
+          local filename = vim.fn.fnamemodify(self.filename, ':.')
+          if filename == '' then
+            return '[No Name]'
+          end
+          if not conditions.width_percent_below(#filename, 0.4) then
+            filename = vim.fn.pathshorten(filename)
+          end
+          return filename
         end,
-        provider = ' [ro]',
-        hl = { fg = utils.get_highlight('DiffChange').fg },
+      },
+      {
+        hl = { fg = palette('green') },
+        {
+          condition = function()
+            return vim.bo.modified
+          end,
+          provider = ' [+]',
+        },
+        {
+          condition = function()
+            return not vim.bo.modifiable or vim.bo.readonly
+          end,
+          provider = ' [ro]',
+        },
       },
     }
-
-    FileNameBlock = utils.insert(FileNameBlock, FileIcon, FileName, FileFlags)
 
     local Git = {
       condition = conditions.is_git_repo,
@@ -125,7 +113,7 @@ return {
           and (self.status_dict.added == nil or self.status_dict.removed == nil or self.status_dict.changed == nil)
       end,
 
-      hl = { fg = utils.get_highlight('Constant').fg },
+      hl = { fg = palette('orange'), bold = true },
 
       {
         provider = function(self)
@@ -135,49 +123,46 @@ return {
             return ' [No HEAD]'
           end
         end,
-        hl = { bold = true },
       },
       {
         condition = function(self)
           return self.has_changes
         end,
         provider = '[',
-        hl = { bold = true },
       },
       {
         condition = function(self)
           return self.untracked
         end,
+        hl = { fg = palette('red') },
         provider = 'untracked',
-        hl = { fg = utils.get_highlight('DiffDelete').fg, bold = true },
       },
       {
+        hl = { fg = palette('green'), bold = false },
         provider = function(self)
           local count = self.status_dict.added or 0
           return count > 0 and ('+' .. count)
         end,
-        hl = { fg = utils.get_highlight('DiffAdd').fg },
       },
       {
+        hl = { fg = palette('red'), bold = false },
         provider = function(self)
           local count = self.status_dict.removed or 0
           return count > 0 and ('-' .. count)
         end,
-        hl = { fg = utils.get_highlight('DiffDelete').fg },
       },
       {
+        hl = { fg = palette('blue'), bold = false },
         provider = function(self)
           local count = self.status_dict.changed or 0
           return count > 0 and ('~' .. count)
         end,
-        hl = { fg = utils.get_highlight('DiffChange').fg },
       },
       {
         condition = function(self)
           return self.has_changes
         end,
         provider = ']',
-        hl = { bold = true },
       },
     }
 
@@ -188,9 +173,12 @@ return {
         self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
         self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
       end,
+
       update = { 'BufEnter', 'LspAttach', 'LspDetach', 'DiagnosticChanged' },
+
       {
         condition = conditions.lsp_attached,
+        hl = { fg = palette('green') },
         {
           provider = function()
             local names = {}
@@ -205,46 +193,45 @@ return {
             return ']'
           end,
         },
-        hl = { fg = utils.get_highlight('DiffAdd').fg },
       },
       {
         condition = conditions.has_diagnostics,
         hl = { bold = true },
         {
+          hl = { bold = false },
           provider = function()
             return '['
           end,
-          hl = { bold = false },
         },
         {
+          hl = { fg = palette('red') },
           provider = function(self)
             return self.errors > 0 and ' E:' .. self.errors
           end,
-          hl = { fg = utils.get_highlight('DiagnosticError').fg },
         },
         {
+          hl = { fg = palette('yellow') },
           provider = function(self)
             return self.warnings > 0 and ' W:' .. self.warnings
           end,
-          hl = { fg = utils.get_highlight('DiagnosticWarn').fg },
         },
         {
+          hl = { fg = palette('blue') },
           provider = function(self)
             return self.info > 0 and ' I:' .. self.info
           end,
-          hl = { fg = utils.get_highlight('DiagnosticInfo').fg },
         },
         {
+          hl = { fg = palette('green') },
           provider = function(self)
             return self.hints > 0 and ' H:' .. self.hints
           end,
-          hl = { fg = utils.get_highlight('DiagnosticHint').fg },
         },
         {
+          hl = { bold = false },
           provider = function()
             return ' ]'
           end,
-          hl = { bold = false },
         },
       },
     }
@@ -255,18 +242,15 @@ return {
 
     require('heirline').setup({
       statusline = {
-        ViMode,
-        SPACE,
+        Mode,
+        { provider = ' ' },
         Git,
-        SPRING,
-        FileNameBlock,
-        SPRING,
+        { provider = '%=' },
+        FileBlock,
+        { provider = '%=' },
         LSP,
         Ruler,
-        hl = { bg = 'bg' },
-      },
-      opts = {
-        colors = colors,
+        hl = { bg = palette('bg1'), fg = palette('fg0') },
       },
     })
   end,
