de545c4909f1b3cf586a1effc411d023f4b821ca
Denis Borsukov <borsukov@proton.me>
2026-06-03 17:38:32 +0300

    multi os support

--- run_once_install-nerdfont.sh
+++ run_once_install-nerdfont.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y curl
-
 curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
-
 ~/.local/bin/getnf -i IosevkaTerm
--- run_once_setup-mpv.sh
+++ run_once_setup-mpv.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y curl wget unzip
-
 mkdir -p ~/.config/mpv/scripts
 
 # uosc
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/tomasklaen/uosc/HEAD/installers/unix.sh)"
-
-# misc
+# thumbfast
 wget -P ~/.config/mpv/scripts https://raw.githubusercontent.com/po5/thumbfast/refs/heads/master/thumbfast.lua
+# autoload
 wget -P ~/.config/mpv/scripts https://raw.githubusercontent.com/mpv-player/mpv/refs/heads/master/TOOLS/lua/autoload.lua
